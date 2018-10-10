787c4ce370312b4661bfd066dfc0c1c9f6f69575
chosenoneMAC <15521370865@163.com>
2018-10-10 22:55:20 +0800

    增加basehandler，配置文件

---
+++ my_blog/config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/10/10 22:16
+
+from tornado.options import options, parse_config_file, parse_command_line
+import motor
+
+import logging
+
+
+# log config
+FORMAT = '[%(asctime)s] [%(levelname)s] %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger(__name__)
+
+
+def load_options(args=None):
+    parse_command_line(args=args)
+    if options.config_file:
+        parse_config_file(options.config_file)
+
+def connect_db():
+    logger.info("mongodb_uri is: {0}".format(options.mongodb_uri))
+    return motor.MotorClient(options.mongodb_uri)
