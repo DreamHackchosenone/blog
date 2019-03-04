a03d8456b5d8a670c8f873c94f8e0154fcd719fb
DreamHackchosenone <15521370865@163.com>
2019-03-04 18:14:47 +0800

    将article_detail.html的js写入detail.js文件

--- static/js/detail.js
+++ static/js/detail.js
@@ -1,21 +1,8 @@
 $(document).ready(function () {
-    var Editor;
-    Editor = editormd("editormd", {
-            width: "90%",
-            height: 640,
-            syncScrolling: "single",
-            emoji: true,
-            path: "../editormd/lib/",
-            /*
-                // 开启上传图片功能
-                imageUpload : true,
-                imageFormats : ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
-                imageUploadURL : "/upload/image/",
-                */
-            saveHTMLToTextarea: true,    // 保存 HTML 到 Textarea
-            searchReplace: true,
-            flowChart: true,             // 开启流程图支持，默认关闭
-            sequenceDiagram: true       // 开启时序/序列图支持，默认关闭,
-        });
-
+    var testEditormdView;
+    testEditormdView = editormd.markdownToHTML("test-editormd-view", {
+    htmlDecode      : "style,script,iframe",  // you can filter tags decode
+    tocm            : true,    // Using [TOCM]
+    emoji           : true,
+  });
 })
